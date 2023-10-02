4ac6b009df327ea78525e1a340ac67fb78c8598a
redazgoura <zgoura.reda@gmail.com>
2023-10-02 14:17:00 +0100

    nav-NavigationHostFragment-action

--- app/src/main/java/com/example/android/navigation/TitleFragment.kt
+++ app/src/main/java/com/example/android/navigation/TitleFragment.kt
@@ -6,18 +6,24 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
+import androidx.navigation.Navigation
 import com.example.android.navigation.databinding.FragmentTitleBinding
 
 
 class TitleFragment : Fragment() {
 
+    private lateinit var binding: FragmentTitleBinding
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        val binding: FragmentTitleBinding = DataBindingUtil.inflate(
+        binding = DataBindingUtil.inflate(
                inflater, R.layout.fragment_title, container, false
         )
+        binding.playButton.setOnClickListener {
+            view: View ->  Navigation.findNavController(view).navigate(R.id.action_titleFragment_to_gameFragment)
+
+        }
             return binding.root
     }
 }
